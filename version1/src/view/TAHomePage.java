fef5b7cf30b99c9cce057d2beb2ffefaafbf7cbf
ahuo-ahuo <dpbla@icloud.com>
2026-03-26 22:28:04 +0800

    Create TAHomePage.java

---
+++ version1/src/view/TAHomePage.java
@@ -0,0 +1,8 @@
+package view;
+
+public class TAHomePage {
+    public static void show() {
+        System.out.println("\n=== TA Portal ===");
+        System.out.println("Welcome, TA user!");
+    }
+}
