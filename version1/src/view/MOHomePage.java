284b6b6f1f6c1cf3d7eb530dd60471cad5e9ade9
ahuo-ahuo <dpbla@icloud.com>
2026-03-26 22:28:42 +0800

    Create MOHomePage.java

---
+++ version1/src/view/MOHomePage.java
@@ -0,0 +1,8 @@
+package view;
+
+public class MOHomePage {
+    public static void show() {
+        System.out.println("\n=== MO Portal ===");
+        System.out.println("Welcome, MO user!");
+    }
+}
