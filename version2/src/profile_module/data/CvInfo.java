f54b614ee40f6711ba6a1ca8f34a1a3fba79c021
Jingwei-Xu <1027754660@qq.com>
2026-04-12 01:04:16 +0800

    Delete version2/src directory

--- version2/src/profile_module/data/CvInfo.java
+++
@@ -1,9 +0,0 @@
-package profile_module.data;
-
-public final class CvInfo {
-    public String fileName = "";
-    public String status = "";
-    public String lastUpdated = "";
-    public String sizeLabel = "";
-}
-
